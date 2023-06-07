9b55a17498872498207f3a87cdc139e41bfa87bd
Eduardo RR <canciondfuego0@gmail.com>
2023-06-07 16:00:58 -0600

    usuario admin

--- Usuarios/UsuarioAdmin.mongodb.js
+++ Usuarios/UsuarioAdmin.mongodb.js
@@ -1,8 +1,42 @@
-db.createUser({
-    user: " MyTester",
-    pwd: passwordPrompt(),
+//!Habilitar autentificacion
+/* 
+?Hay que meterse al archivo mongod.cfg y copiar y pegar lo siguiente:
+*security:
+*    authorization: enabled
+*/
+
+
+use("admin");
+
+db.createUser(
+  {
+    user: "myUserAdmin",
+    pwd: passwordPrompt(), // or cleartext password
     roles: [
-        { role: "readWrite", db: "test" },
-        { role: "read", db: "reporting" },
-    ],
-});
+      { role: "userAdminAnyDatabase", db: "admin" },
+      { role: "readWriteAnyDatabase", db: "admin" }
+    ]
+  }
+)
+
+/**
+ * ?Autentificacion durante la conexion:
+ * * mongosh --port 27017  --authenticationDatabase "admin" -u "myUserAdmin" -p
+ * 
+ * ?Autentificacion antes de la conexion:
+ * *use admin
+ * *db.auth("myUserAdmin", passwordPrompt()) // or cleartext password
+ */
+
+/**
+ * !Contraseña usuario admin:
+ * ? hola1234
+ */
+
+
+
+
+/**
+ * !Bibliografia
+ * * Crear usuario admin: https://www.mongodb.com/docs/manual/tutorial/configure-scram-client-authentication/#std-label-create-user-admin
+ */
